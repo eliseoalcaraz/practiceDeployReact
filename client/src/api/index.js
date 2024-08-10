868f7f04b77f4452e2fbbe2f5e1cd78c164a38d1
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-08-10 13:08:11 +0800

    changed the backend url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000';
+const url =  'https://practicedeployreact.onrender.com';
 
 // Existing receipt-related API calls
 export const fetchReceipts = () => axios.get(`${url}/receipts`);
